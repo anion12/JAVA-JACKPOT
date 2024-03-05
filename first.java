957a3fe48097e2ef4096405a916ba7c0b785e0b7
anion12 <120101251+anion12@users.noreply.github.com>
2024-03-05 10:55:50 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,20 @@
+class hello
+{
+    public static void main(String a[] ){
+    System.out.println("hello world!");//we use println because to get on the new line
+    int num1 = 5;
+    byte num2 = 7;
+    short sh = 558;
+    long l = 5678l;
+
+    float f = 5.9f;
+    double d = 4.98;
+
+    char c = 'k';//even double quotes also gives error
+    boolean b = true;
+
+
+    System.out.println(num1+num2);
+    }
+}
+
