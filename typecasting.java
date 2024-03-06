43ffeb0cd2657e9a020f8770bd4fe4d62104713c
anion12 <120101251+anion12@users.noreply.github.com>
2024-03-06 12:05:52 +0530

    Update typecasting.java

--- typecasting.java
+++ typecasting.java
@@ -1,3 +1,9 @@
+/*TYPE CONVERSION AND TYPE CASTING*/
+/*TYPE CONVERSION: EXPLICITLY DOESNT NEED TO CHANGE THE DATATYPE BY CASTING.LETS SAY AN INT a and byte b then a = b doesnt give us 
+ error. 
+ 
+ TYPE CASTING: casting the tyoe we want to convert it into is reffered to as type casting.examples are given below*/
+
 class hi{
     public static void main(String s[]){
         byte b = 124;
