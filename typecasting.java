3ff8aca4a71f7f238bff5a4f2d05577c7369d68b
anion12 <120101251+anion12@users.noreply.github.com>
2024-03-06 12:01:19 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,15 @@
+class hi{
+    public static void main(String s[]){
+        byte b = 124;
+        int a = 500;
+        byte k = (byte)a;
+        
+
+
+    float f = 5.6f;
+    int t = (int) f;
+        System.out.println(t);
+        System.out.println(k);
+
+    }
+}
