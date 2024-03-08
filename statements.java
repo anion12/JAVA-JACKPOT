1dafdf511c83ee5125a355623a824a0b278d4d2e
anion12 <120101251+anion12@users.noreply.github.com>
2024-03-08 19:10:15 +0530

    Add files via upload

---
+++ statements.java
@@ -0,0 +1,9 @@
+public class statements {
+    public static void main(String[] args){
+        int x = 7;
+        if(x>5 && x%2 == 0)
+        {     System.out.print("hello");}
+        else
+             {System.out.println("boi boi");}
+    }
+}
