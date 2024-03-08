c3283267a55ff0388009d7f83362aa6589a7e34b
anion12 <120101251+anion12@users.noreply.github.com>
2024-03-08 19:14:16 +0530

    Add files via upload
    this is an example of where we use conditional statements for finding the largest among 3 numbers.

---
+++ greatest.java
@@ -0,0 +1,15 @@
+public class greatest {
+    public static void main(String[] args)
+    {
+        int x = 17;
+        int y = 25;
+        int z = 8;
+
+        if(x>y && x>z)
+            System.out.println(x);
+        else if(y>z)
+            System.out.println(y);
+        else
+            System.out.println(z);
+    }
+}
