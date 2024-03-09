087011ddfda39e10ad142174c9748946d61638a4
anion12 <120101251+anion12@users.noreply.github.com>
2024-03-09 19:29:23 +0530

    Add files via upload

---
+++ switch.java
@@ -0,0 +1,30 @@
+ class easy {
+    public static void main(String a[])
+    {
+        int x = 5;
+        switch(x){
+            case 1:
+            System.out.println("monday");
+            break;
+             case 2:
+            System.out.println("tuesday");
+            break; 
+            case 3:
+            System.out.println("wednesday");
+            break;
+            case 4:
+            System.out.println("thursday");
+            break; 
+            case 5:
+            System.out.println("friday");
+            break; 
+            case 6:
+            System.out.println("saturday");
+            break; 
+            case 7:
+            System.out.println("sunday");
+            default:
+            System.out.println("enter a valid number");
+        }
+    }
+}
