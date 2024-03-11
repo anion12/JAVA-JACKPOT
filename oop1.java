35e76be710a5c837c062b1bc320f607becf68fae
anion12 <120101251+anion12@users.noreply.github.com>
2024-03-11 19:34:31 +0530

    Add files via upload

---
+++ oop1.java
@@ -0,0 +1,29 @@
+class calculator{
+    
+    int num = 5;
+    public int add(int n1, int n2)
+    {
+       int r = n1+ n2;
+        return r;
+    }
+    public void add(int n1, int n2, int n3)
+    {
+        System.out.println(n1+n2+n3);
+    }
+}
+
+public class oop1 {
+    public static void main(String[] args)
+    {
+        int num1 = 4;
+        int num2 = 5;
+        int data = 10;
+        calculator calc = new calculator();
+      int  result = calc.add(num1,num2);
+      calc.add(1,2,3);
+      System.out.println(result);
+      /*StringBuffer sb = new StringBuffer();
+      System.out.println(sb);*/
+     
+    }
+}
