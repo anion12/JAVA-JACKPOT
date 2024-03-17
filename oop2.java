c65d9d32ea7140e7c10a0269e42859f25cd6b4e1
anion12 <120101251+anion12@users.noreply.github.com>
2024-03-17 11:14:05 +0530

    Add files via upload

---
+++ oop2.java
@@ -0,0 +1,25 @@
+class simp
+        {
+            public
+            void playmusic()
+            {
+                System.out.println("music playing");
+            }
+            public String getmeapen(int cost)
+            {
+                if(cost>10)
+                  return "pen";
+                return"nothing";/*as it is in method eventhough no else 
+                is provided it still works as if else statement */
+            
+            }
+        }
+public class oop2 {
+    public static void main(String[] args)
+    {
+        simp obj = new simp();
+        obj.playmusic();
+       String str= obj.getmeapen(2);
+       System.out.println(str);
+    }
+}
